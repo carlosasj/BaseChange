d091811e961a9814add0600401c23d2c48bee3d6
Lucas <lucas.crocomo@gmail.com>
2015-05-13 15:23:23 -0300

    printing messages and reading values added, and working

--- 1-trab_teste_2015-05-12.asm
+++ 1-trab_teste_2015-05-12.asm
@@ -1,16 +1,48 @@
 .data
-.align 0
-strInfoBase: .asciiz "As bases possiveis para esse programa sao:\n2 - Binario\n8 - Octal\n10 - Decimal\n16 - Hexadecimal"
-strGetBaseIn: .asciiz "Qual será a base de entrada? "
-strGetBaseOut: .asciiz "Qual será a base de saida? "
+.align 2
+strInfoBase: .asciiz "As bases possiveis para esse programa sao:\n2 - Binario\n8 - Octal\n10 - Decimal\n16 - Hexadecimal\n"
+strGetBaseIn: .asciiz "Qual sera a base de entrada? "
+strGetBaseOut: .asciiz "Qual sera a base de saida? "
 strOut1: .asciiz "O Numero "
 strOut2: .asciiz " da base "
-baseIn: .word 0
-strOut2: .asciiz " para a base "
+baseIn: .word 15
+strOut3: .asciiz " para a base "
 baseOut: .word 0
-strOut2: .asciiz " eh "
+strOut4: .asciiz " eh "
 
 binary: .word 0:31
 input: .space 32
 
 .text
+
+main:
+
+	# Printing the possible bases to convert to
+	li $v0, 4  		# 4 is the code for printing a string
+	la $a0, strInfoBase 	# loading the string to be printed
+	syscall			# requesting the system to print the message
+	
+	# Asking the user to enter baseIn
+	li $v0, 4  		# 4 is the code for printing a string
+	la $a0, strGetBaseIn 	# loading the string to be printed
+	syscall			# requesting the system to print the ask message
+	
+	
+	# Reading baseIn (int)
+	li $v0, 5		# 5 is the code for reading a int
+	syscall 		# requesting the system to read baseIn
+	# Storing baseIn
+	move $t1, $v0		# copying content in v0 to t1
+	sw $t1, baseIn		# storing the value in the memory
+	
+	# Asking the user to enter baseOut
+	li $v0, 4  		# 4 is the code for printing a string
+	la $a0, strGetBaseOut 	# loading the string to be printed
+	syscall			# requesting the system to print the ask message
+	
+	# Reading baseOut (int)
+	li $v0, 5		# 5 is the code for reading a int
+	syscall 		# requesting the system to read baseOut
+	# Storing baseOut
+	move $t1, $v0		# copying content in v0 to t1
+	sw $t1, baseOut		# storing the value in the memory
